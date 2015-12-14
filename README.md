b198e5d94bb6db613ee43567c927bcf0ba8e550c
Tyler Rivera <riverat2@email.chop.edu>
2015-12-13 20:42:46 -0500

    📦

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a simple script designed to update a DNS record for a domain administered on DigitalOcean with the current IP of the machine the script runs on. This is particularly useful if you're running a server from your home with a changing IP.
+
+Required environment variables:
+
+`DIGITALO_API_KEY`: Your DigitalO API Token
+`DIGITALO_DOMAIN_NAME`: The associated domain name (i.e. `example.com`)
+`DIGITALO_DOMAIN_RECORD_ID`: The domain record id that you wish to keep updated.
---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import requests
+from digitalocean import Domain, Record
+
+API_TOKEN = os.environ('DIGITALO_API_KEY', '')
+DOMAIN_NAME = os.environ('DIGITALO_DOMAIN_NAME', '')
+DOMAIN_RECORD_ID = os.environ('DIGITALO_DOMAIN_RECORD_ID', '')
+
+
+def main():
+    domain = Domain().get_object(API_TOKEN, DOMAIN_NAME)
+    record = Record().get_object(API_TOKEN, domain, DOMAIN_RECORD_ID)
+
+    ip = requests.get('http://icanhazip.com').text.strip()
+
+    record.data = ip
+
+    record.save()
+
+if __name__ == '__main__':
+    main()
